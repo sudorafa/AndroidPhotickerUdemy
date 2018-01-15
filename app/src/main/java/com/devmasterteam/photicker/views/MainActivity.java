c478a2bafbfc1944185d72d5821c45165f537b4a
Rafa Lima <paracafa@gmail.com>
2018-01-15 14:46:28 -0300

    Aula 51 - Colocando adesivo no meio quando clicado na escolha.

--- app/src/main/java/com/devmasterteam/photicker/views/MainActivity.java
+++ app/src/main/java/com/devmasterteam/photicker/views/MainActivity.java
@@ -1,9 +1,12 @@
 package com.devmasterteam.photicker.views;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.devmasterteam.photicker.R;
 
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         List<Integer> mListImages = ImageUtil.getImageList();
 
+        final RelativeLayout relativeLayout = (RelativeLayout) this.findViewById(R.id.relative_photo_content_draw);
         final LinearLayout content = (LinearLayout) this.findViewById(R.id.linear_horizontal_scroll);
 
         for(Integer imageId : mListImages){
@@ -31,8 +35,33 @@ public class MainActivity extends AppCompatActivity {
             image.setImageBitmap(ImageUtil.decodeSampledBitmapFromResource(getResources(), imageId, 70, 70));
             image.setPadding(20, 10, 20, 10);
 
+            BitmapFactory.Options dimensions = new BitmapFactory.Options();
+            dimensions.inJustDecodeBounds = true;
+            BitmapFactory.decodeResource(getResources(), imageId, dimensions);
+
+            final int width = dimensions.outWidth;
+            final int height = dimensions.outHeight;
+
+            image.setOnClickListener(onClickImageOption(relativeLayout, imageId, width, height));
+
             content.addView(image);
         }
 
+    }
+
+    private View.OnClickListener onClickImageOption(final RelativeLayout relativeLayout, final Integer imageId, final int width, final int height) {
+        return new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                final ImageView image = new ImageView(MainActivity.this);
+                image.setBackgroundResource(imageId);
+                relativeLayout.addView(image);
+
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) image.getLayoutParams();
+                layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+                layoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
+            }
+        };
     }
 }
