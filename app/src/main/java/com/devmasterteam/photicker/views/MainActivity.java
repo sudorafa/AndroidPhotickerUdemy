6209c2579cee199a153286bd2fff6643dfdb189a
Rafa Lima <paracafa@gmail.com>
2018-01-15 14:32:56 -0300

    Aula 50 - Criado class com pacote util para incluir os adesivos na horizontal scroll view

--- app/src/main/java/com/devmasterteam/photicker/views/MainActivity.java
+++ app/src/main/java/com/devmasterteam/photicker/views/MainActivity.java
@@ -2,8 +2,14 @@ package com.devmasterteam.photicker.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.devmasterteam.photicker.R;
+
+import java.util.List;
+
+import utils.ImageUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        List<Integer> mListImages = ImageUtil.getImageList();
+
+        final LinearLayout content = (LinearLayout) this.findViewById(R.id.linear_horizontal_scroll);
+
+        for(Integer imageId : mListImages){
+            ImageView image = new ImageView(this);
+            image.setImageBitmap(ImageUtil.decodeSampledBitmapFromResource(getResources(), imageId, 70, 70));
+            image.setPadding(20, 10, 20, 10);
+
+            content.addView(image);
+        }
 
     }
 }
---
+++ app/src/main/java/utils/ImageUtil.java
@@ -0,0 +1,133 @@
+package utils;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.devmasterteam.photicker.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by oRafa on 15/01/2018.
+ */
+
+public class ImageUtil {
+    public static List<Integer> getImageList(){
+        List<Integer> images = new ArrayList<>();
+        images.add(R.drawable.st_facial_0);
+        images.add(R.drawable.st_facial_1);
+        images.add(R.drawable.st_facial_3);
+        images.add(R.drawable.st_facial_5);
+        images.add(R.drawable.st_facial_4);
+        images.add(R.drawable.st_facial_6);
+        images.add(R.drawable.st_facial_7);
+        images.add(R.drawable.st_facial_8);
+        images.add(R.drawable.st_facial_10);
+        images.add(R.drawable.st_facial_11);
+        images.add(R.drawable.st_facial_12);
+        images.add(R.drawable.st_facial_13);
+        images.add(R.drawable.st_facial_14);
+        images.add(R.drawable.st_misc_1);
+        images.add(R.drawable.st_objeto_2);
+        images.add(R.drawable.st_objeto_3);
+        images.add(R.drawable.st_objeto_4);
+        images.add(R.drawable.st_objeto_5);
+        images.add(R.drawable.st_animal_0);
+        images.add(R.drawable.st_animal_2);
+        images.add(R.drawable.st_animal_3);
+        images.add(R.drawable.st_animal_4);
+        images.add(R.drawable.st_animal_5);
+        images.add(R.drawable.st_animal_6);
+        images.add(R.drawable.st_animal_7);
+        images.add(R.drawable.st_animal_8);
+        images.add(R.drawable.st_animal_10);
+        images.add(R.drawable.st_animal_11);
+        images.add(R.drawable.st_animal_12);
+        images.add(R.drawable.st_animal_13);
+        images.add(R.drawable.st_animal_14);
+        images.add(R.drawable.st_animal_16);
+        images.add(R.drawable.st_animal_17);
+        images.add(R.drawable.st_animal_18);
+        images.add(R.drawable.st_animal_19);
+        images.add(R.drawable.st_animal_20);
+        images.add(R.drawable.st_animal_21);
+        images.add(R.drawable.st_animal_22);
+        images.add(R.drawable.st_animal_23);
+        images.add(R.drawable.st_animal_24);
+        images.add(R.drawable.st_animal_25);
+        images.add(R.drawable.st_comida_1);
+        images.add(R.drawable.st_comida_2);
+        images.add(R.drawable.st_comida_3);
+        images.add(R.drawable.st_comida_5);
+        images.add(R.drawable.st_comida_6);
+        images.add(R.drawable.st_coracao_1);
+        images.add(R.drawable.st_coracao_2);
+        images.add(R.drawable.st_coracao_3);
+        images.add(R.drawable.st_coracao_4);
+        images.add(R.drawable.st_coracao_5);
+        images.add(R.drawable.st_coracao_6);
+        images.add(R.drawable.st_drink_1);
+        images.add(R.drawable.st_drink_2);
+        images.add(R.drawable.st_drink_3);
+        images.add(R.drawable.st_drink_4);
+        images.add(R.drawable.st_drink_5);
+        images.add(R.drawable.st_drink_6);
+        images.add(R.drawable.st_drink_7);
+        images.add(R.drawable.st_drink_8);
+        images.add(R.drawable.st_drink_9);
+        images.add(R.drawable.st_drink_10);
+        images.add(R.drawable.st_tatto_1);
+        images.add(R.drawable.st_tatto_2);
+        images.add(R.drawable.st_tatto_3);
+        images.add(R.drawable.st_tatto_4);
+        images.add(R.drawable.st_tatto_5);
+        images.add(R.drawable.st_tatto_6);
+        images.add(R.drawable.st_sticker_2);
+        images.add(R.drawable.st_sticker_5);
+        images.add(R.drawable.st_sticker_6);
+        images.add(R.drawable.st_sticker_7);
+        images.add(R.drawable.st_sticker_8);
+        images.add(R.drawable.st_sticker_9);
+        images.add(R.drawable.st_sticker_11);
+
+        return images;
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+
+        // Largura e altura da imagem
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calcula o maior valor inSampleSize que é uma potência de 2 e mantém altura e largura maior do que o necessário
+            while ((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+
+        // Primeiro decodifica para verificar as dimensões
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calcula inSampleSize - O valor correspondende para a imagem ser redimensionada
+        options.inSampleSize = ImageUtil.calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decodifica imagem usando o valor calculado
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+}
