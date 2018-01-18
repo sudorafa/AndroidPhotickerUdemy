ec2f9e2980b682e13d90606e02101d41da04ea10
Rafa Lima <paracafa@gmail.com>
2018-01-18 11:54:33 -0300

    Aula 54 - Movimentar imagem escolhida na tela

--- app/src/main/java/com/devmasterteam/photicker/views/MainActivity.java
+++ app/src/main/java/com/devmasterteam/photicker/views/MainActivity.java
@@ -3,6 +3,7 @@ package com.devmasterteam.photicker.views;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -90,6 +91,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 toogleControlPanel(true);
 
                 mImageSelected = image;
+
+                image.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                        float x, y;
+                        switch (motionEvent.getAction()) {
+                            case MotionEvent.ACTION_DOWN:
+                                mImageSelected = image;
+                                toogleControlPanel(true);
+                                break;
+                            case MotionEvent.ACTION_MOVE:
+
+                                int coords[] = {0, 0};
+                                relativeLayout.getLocationOnScreen(coords);
+
+                                x = (motionEvent.getRawX() - (image.getWidth() / 2));
+                                y = motionEvent.getRawY() - ((coords[1] + 100) + (image.getHeight() / 2));
+                                image.setX(x);
+                                image.setY(y);
+
+                                break;
+                            case MotionEvent.ACTION_UP:
+                                break;
+                        }
+                        return true;
+                    }
+                });
             }
         };
     }
